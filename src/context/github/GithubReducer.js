6d4c9eb125f34a863f657adb754dfbe7b60cb11d
KyleCagayan <43043797+KyleCagayan@users.noreply.github.com>
2022-10-13 15:25:15 -0400

    full project

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,40 @@
+const githubReducer = (state, action) => {
+  // action is just a string
+
+  switch (action.type) {
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+        loading: false,
+      };
+
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case "GET_USER":
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
+    case "GET_REPOS":
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false,
+      };
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default githubReducer;
